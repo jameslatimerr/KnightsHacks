9bd626dbe2d90508ae4a5c6ed9ab57557376c240
Natnael Tewolde <94206635+blacknael@users.noreply.github.com>
2021-11-14 17:37:04 -0500

    Added improved input functionality for calculating

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,44 +1,26 @@
 <template>
   <div class="home">
     <h1>{{calorie_count}}</h1>
-    <input v-model.number="calorie_sub">
-    <button type="button" class="btn btn-primary" v-on:click=result()>Subtract Calories</button>
-    <modal v-if="(modalData.showModal = true) && (/[a-zA-Z]/).test(calorie_sub)" @click = "popModal"></modal>
-    <modal v-show = "showModal" @close = "closeModal"></modal>
-    <br><br>
-    <input v-model.number="calorie_add">
-    <button type="button" class="btn btn-primary" v-on:click=add()>Add Calories</button>
-    <modal v-if="(modalData.showModal = true) && (/[a-zA-Z]/).test(calorie_add)" @click = "popModal"></modal>
-    <modal v-show = "showModal" @close = "closeModal"></modal>
+    <form>
+      <label for = "subcalories">Subtract:</label>
+      <input v-model.number = "calorie_sub" type = "number" min = "0" oninput="validity.valid||(value='');">
+      <button type= "button" class= "btn btn-primary" @click=result()>Subtract Calories</button>
+      <br><br>
+      <label for = "addcalories">Add:</label>
+      <input v-model.number = "calorie_add" type = "number" min = "0" oninput="validity.valid||(value='');">
+      <button type= "button" class= "btn btn-primary" @click=add()>Add Calories</button>
+    </form>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import modal from '@/components/modal.vue'
 import Vue from 'vue'
-/*
-Vue.component('calorie_subtracter', {
-  data: function () {
-    <vue-number-input v-model="value"  inline controls></vue-number-input>
 
-    return {
-      count: 0
-    }
-
-  },
-  template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
-})
-*/
 export default {
   name: 'Home',
-  components: {modal},
   data(){
     return{
-      modalData: {
-        showModal: false,
-        modalHeader: "Enter a definition an word",
-      },
       calorie_count: 3000,
       calorie_sub : 0,
       calorie_add: 0,
@@ -60,16 +42,8 @@ export default {
       set_sub: function(num) {
         this.calorie_sub = parseInt(num);
       }
-    };
-  },
-  methods: {
-    popModal() {
-      this.showModal = true;
-    },
-    closeModal() {
-      this.showModal = false;
     }
-  },
-};
+  }
+}
 </script>
 
