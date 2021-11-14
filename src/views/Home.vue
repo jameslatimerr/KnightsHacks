85af19240f4eaae730100db71226921c895f111f
Natnael Tewolde <94206635+blacknael@users.noreply.github.com>
2021-11-14 16:41:58 -0500

    Incorporated modals for the Add Calorie feature

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="home">
-    <h1>Calorie Calculator</h1>
-    <h2>{{calorie_count}}</h2>
+    <h1>{{calorie_count}}</h1>
     <input v-model.number="calorie_sub">
-    <h1></h1>
     <button type="button" class="btn btn-primary" v-on:click=result()>Subtract Calories</button>
-    <h1></h1>
+    <modal v-if="(modalData.showModal = true) && (/[a-zA-Z]/).test(calorie_sub)" @click = "popModal"></modal>
+    <modal v-show = "showModal" @close = "closeModal"></modal>
+    <br><br>
     <input v-model.number="calorie_add">
-    <h1></h1>
     <button type="button" class="btn btn-primary" v-on:click=add()>Add Calories</button>
+    <modal v-if="(modalData.showModal = true) && (/[a-zA-Z]/).test(calorie_add)" @click = "popModal"></modal>
+    <modal v-show = "showModal" @close = "closeModal"></modal>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import modal from '@/components/modal.vue'
 import Vue from 'vue'
 /*
 Vue.component('calorie_subtracter', {
@@ -30,29 +32,44 @@ Vue.component('calorie_subtracter', {
 */
 export default {
   name: 'Home',
+  components: {modal},
   data(){
     return{
+      modalData: {
+        showModal: false,
+        modalHeader: "Enter a definition an word",
+      },
       calorie_count: 3000,
       calorie_sub : 0,
       calorie_add: 0,
       //num: 0,
       result: function () {
-   this.calorie_count = parseInt(this.calorie_count) - parseInt(this.calorie_sub);
-   if(this.calorie_count < 0){
-     this.calorie_count = 0;
-   }
-    },
-    add: function(){
-      this.calorie_count = parseInt(this.calorie_count) + parseInt(this.calorie_add);
-    },
-    set_sub: function(num) {
-      this.calorie_sub = parseInt(num);
-    },
-    types: [
-          'search',
-        ]
-    }
+      
+          this.calorie_count = parseInt(this.calorie_count) - parseInt(this.calorie_sub);
+          if(this.calorie_count < 0){
+            this.calorie_count = 0;
+        }
+      },
+      add: function(){
 
-  }
-}
+        this.calorie_count = parseInt(this.calorie_count) + parseInt(this.calorie_add);
+          if(this.calorie_count < 0){
+            this.calorie_count = 0;
+          }
+      },
+      set_sub: function(num) {
+        this.calorie_sub = parseInt(num);
+      }
+    };
+  },
+  methods: {
+    popModal() {
+      this.showModal = true;
+    },
+    closeModal() {
+      this.showModal = false;
+    }
+  },
+};
 </script>
+
